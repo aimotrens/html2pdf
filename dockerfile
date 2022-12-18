a928538c888db584309eea582f86936fc2b7a81c
Timo Ahrends <T_Ahrends@t3a6.de>
2022-12-18 18:35:58 +0100

    Initial Code Commit

---
+++ dockerfile
@@ -0,0 +1,44 @@
+FROM debian:bullseye as fonts
+RUN echo 'deb http://deb.debian.org/debian bullseye main contrib non-free' > /etc/apt/sources.list
+RUN apt-get update && apt-get install -y --no-install-recommends ca-certificates ttf-mscorefonts-installer 
+
+# ---
+
+FROM debian:bullseye as wkhtmltopdf
+RUN apt-get update && apt-get install -y --no-install-recommends wget ca-certificates
+RUN wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
+RUN apt-get install -y --no-install-recommends /wkhtmltox_0.12.6.1-2.bullseye_amd64.deb
+
+# ---
+
+FROM golang:1.19 as builder
+WORKDIR /build
+COPY . .
+
+# swaggo "installieren" um die Swagger Doku zu generieren
+RUN curl -L https://github.com/swaggo/swag/releases/download/v1.8.9/swag_1.8.9_Linux_x86_64.tar.gz | \
+    tar xvfz - swag
+
+RUN ./swag init
+RUN go build -o html2pdf .
+
+# ---
+
+FROM debian:bullseye
+WORKDIR /app
+ENV WKHTMLTOPDF_PATH=/usr/bin/wkhtmltopdf
+ENV GIN_MODE=release
+EXPOSE 8080/tcp
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends libjpeg62 libpng16-16 libxrender1 libfontconfig1 && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /usr/share/doc/*
+
+COPY --from=fonts /usr/share/fonts/truetype/msttcorefonts/ /usr/share/fonts/truetype/msttcorefonts/
+COPY --from=wkhtmltopdf /usr/local/bin/wkhtmltopdf /usr/bin/
+COPY --from=builder /build/html2pdf /app/
+
+HEALTHCHECK CMD curl --fail http://localhost:8080/api/healthcheck || exit 1
+
+CMD ["./html2pdf"]
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gofrs/uuid"
+
+	docs "github.com/aimotrens/html2pdf/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+var _wkhtmltopdfPath string
+
+func main() {
+	_wkhtmltopdfPath := os.Getenv("WKHTMLTOPDF_PATH")
+	if _wkhtmltopdfPath == "" {
+		_wkhtmltopdfPath = "./wkhtmltopdf"
+	}
+
+	if _, err := os.Stat(_wkhtmltopdfPath); err != nil {
+		log.Fatal("wkhtmltopdf not found: '" + _wkhtmltopdfPath + "'")
+	}
+
+	docs.SwaggerInfo.Title = "HTML to PDF Konverter"
+	docs.SwaggerInfo.BasePath = "/api"
+
+	r := gin.Default()
+
+	api := r.Group("/api")
+	{
+		api.GET("/healthcheck", HealthCheck)
+		h2p := api.Group("/html2pdf")
+		{
+			h2p.POST("/convert", Convert)
+		}
+	}
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	fmt.Println("Html2Pdf started.")
+	r.Run(":8080")
+}
+
+// Convert HTML to PDF
+// @Summary Konvertiert eine HTML Seite in ein A4 PDF-Dokument
+// @Param request body string true "HTML-Seite"
+// @Accept text/html
+// @Produce  application/pdf
+// @Success 200 {file} binary
+// @Success 500 {string} string
+// @Router /html2pdf/convert [post]
+func Convert(g *gin.Context) {
+	tmpID, err := uuid.NewV4()
+	if err != nil {
+		g.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	tmpFile := "/tmp/html2pdf-" + tmpID.String()
+	defer os.Remove(tmpFile)
+
+	data, err := io.ReadAll(g.Request.Body)
+	if err != nil {
+		g.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	p := exec.Command(_wkhtmltopdfPath, "--page-size", "A4", "-", tmpFile)
+	w, err := p.StdinPipe()
+	if err != nil {
+		g.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	w.Write(data)
+	w.Close()
+
+	p.Stdout = os.Stdout
+	p.Stderr = os.Stderr
+
+	err = p.Start()
+	if err != nil {
+		g.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	p.Wait()
+
+	if ec := p.ProcessState.ExitCode(); ec != 0 {
+		g.AbortWithError(http.StatusInternalServerError, errors.New("wkhtmltopdf failed"))
+		return
+	}
+
+	g.Header("content-type", "application/pdf")
+	g.File(tmpFile)
+}
+
+// Healthcheck
+// @Summary Gibt immer "OK" zurück
+// @Produce  text/plain
+// @Success 200 {string} OK
+// @Router /healthcheck [get]
+func HealthCheck(g *gin.Context) {
+	g.String(http.StatusOK, "OK")
+}
